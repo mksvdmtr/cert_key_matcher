fc9f952c100124c630c3fd46f039de0e590ddc26
Dmitriy <mksvdmtr@yandex.ru>
2019-06-23 12:57:22 +0300

    create script

---
+++ cert_key_matcher.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+cert=$(openssl x509 -in $1 -pubkey -noout -outform pem | sha256sum)
+key=$(openssl pkey -in $2 -pubout -outform pem | sha256sum)
+
+if [ "$1" == "-h" ] || [ -z $1 ] || [ -z $2 ]
+then
+  echo "Usage: `basename $0` {cert} {key}"
+  exit 0
+else
+	if [ "$1" == "$2" ]
+	then
+		printf "\n"
+		printf "${GREEN}Match!\n"
+		printf "\n"
+	else
+		printf "\n"
+		printf "${RED}Not match\n"
+		printf "\n"
+	fi
+fi
